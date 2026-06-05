c669f661c0e5c9c0553d7418babef71188d90095
Yovsky <mouloudguenane651@gmail.com>
2026-06-05 23:31:03 +0100

    feat: add volume, CSA and TSA calculation

--- updated_calculator.cpp
+++ updated_calculator.cpp
@@ -8,7 +8,8 @@ int main(){
     cin>>choice;
 
     
-    if(choice == "basic_calculation"){
+    if(choice == "basic_calculation")
+    {
 
         cout<<"kya karna chate h aap addition,subtraction,division,multipication or area"<<endl;
         cin>>choice;
@@ -62,105 +63,110 @@ int main(){
 
 
 
-        }else if(choice == "area"){
-            cout<<"konsi figure ka area nikalna h ?"<<endl;
-            cin>>choice;
-
-            if(choice == "square"){
-
-                cout<<"enter the side length"<<endl;
-                float side_length;
-                cin>>side_length;
-                float arsquare;
-                arsquare = side_length*side_length;
-                cout<<arsquare<<endl;
-                return 0;
-            }else if(choice == "rectangle"){
-                cout<<"enter the length and breadth"<<endl;
-                float length;
-                float breadth;
-                float arrectangle;
-                cin>>length>>breadth;
-                arrectangle = length*breadth;
-                cout<<arrectangle<<endl;
-                return 0;
-            }else if(choice == "circle"){
-                cout<<"enter the radius"<<endl;
-                float radius;
-                cin>>radius;
-                float pi = 3.14;
-                float arcircle;
-                arcircle = pi*radius*radius;
-                cout<<arcircle<<endl;
-                return 0;
-            }else if(choice == "triangle"){
-                cout<<"enter base and height"<<endl;
-                float base;
-                float height;
-                float artriangle;
-                cin>>base>>height;
-                artriangle = 0.5*base*height;
-                cout<<artriangle<<endl;
-                return 0 ;
-
-            }
-    
-    }else if(choice =="perimeter"){
-   
-      cout<<"select figure rectangle , square ,triangle , circle"<<endl;
-      cin>>choice;
-
-      if(choice == "square"){
-
-        cout<<"enter the side length"<<endl;
-        float side_length;
-        cin>>side_length;
-        float prisquare;
-        prisquare = 4*side_length;
-        cout<<prisquare<<endl;
-        return 0;
-
-      }else if(choice == "rectangle"){
-      
-        float length;
-        float breadth;
-        float prirectangle;
-        cout<<"enter length and breadth"<<endl;
-        cin>>length>>breadth;
-        prirectangle = 2*(length + breadth);
-        cout<<prirectangle<<endl;
-        return 0;
-      }else if(choice == "circle"){
-
-        float radius;
-        cout<<"enter the radius"<<endl;
-        cin>>radius;
-        float pi = 3.14;
-        float pricircle;
-        pricircle = pi*2*radius;
-        cout<<pricircle<<endl;
-        return 0;
-
-      }else if( choice == "triangle"){
-
-    if(a+b>c && b+c>a && a+c>b){
-     float a; 
-     float b;
-     float c;
-     float pritriangle;
-     cout<<"enter the length of all 3 sides"<<endl;
-     cin>>a>>b>>c;
-     pritriangle = a + b + c;
-
-     cout<<pritriangle<<endl;
-     return 0;
-    }else{
-        cout<<"it isn't a triangle"<<endl;
-      }
-
-    }else{
-
-        cout<<"invalid input"<<endl;
     }
-    return 0;
-}
+    else if(choice == "area")
+    {
+        cout<<"konsi figure ka area nikalna h ?"<<endl;
+        cin>>choice;
+        if(choice == "square"){
+            cout<<"enter the side length"<<endl;
+            float side_length;
+            cin>>side_length;
+            float arsquare;
+            arsquare = side_length*side_length;
+            cout<<arsquare<<endl;
+            return 0;
+        }else if(choice == "rectangle"){
+            cout<<"enter the length and breadth"<<endl;
+            float length;
+            float breadth;
+            float arrectangle;
+            cin>>length>>breadth;
+            arrectangle = length*breadth;
+            cout<<arrectangle<<endl;
+            return 0;
+        }else if(choice == "circle"){
+            cout<<"enter the radius"<<endl;
+            float radius;
+            cin>>radius;
+            float pi = 3.14;
+            float arcircle;
+            arcircle = pi*radius*radius;
+            cout<<arcircle<<endl;
+            return 0;
+        }else if(choice == "triangle"){
+            cout<<"enter base and height"<<endl;
+            float base;
+            float height;
+            float artriangle;
+            cin>>base>>height;
+            artriangle = 0.5*base*height;
+            cout<<artriangle<<endl;
+            return 0 ;
+        }
+    }
+    else if(choice =="perimeter")
+    {
+        
+        cout<<"select figure rectangle , square ,triangle , circle"<<endl;
+        cin>>choice;
+        if(choice == "square")
+        {
+            cout<<"enter the side length"<<endl;
+            float side_length;
+            cin>>side_length;
+            float prisquare;
+            prisquare = 4*side_length;
+            cout<<prisquare<<endl;
+            return 0;
+
+        }
+        else if(choice == "rectangle")
+        {
+            float length;
+            float breadth;
+            float prirectangle;
+            cout<<"enter length and breadth"<<endl;
+            cin>>length>>breadth;
+            prirectangle = 2*(length + breadth);
+            cout<<prirectangle<<endl;
+            return 0;
+        }else if(choice == "circle")
+        {
+            float radius;
+            cout<<"enter the radius"<<endl;
+            cin>>radius;
+            float pi = 3.14;
+            float pricircle;
+            pricircle = pi*2*radius;
+            cout<<pricircle<<endl;
+            return 0;
+        }
+        else if( choice == "triangle")
+        {
+            float a;
+            float b;        // variables a, b, and c used in if condition before initialization.
+            float c;
+            float pritriangle;
+            cout<<"enter the length of all 3 sides"<<endl;
+            cin>>a>>b>>c;
+            if(a+b>c && b+c>a && a+c>b){
+            
+            pritriangle = a + b + c;
+
+            cout<<pritriangle<<endl;
+            return 0;
+            }
+            else
+            {
+            cout<<"it isn't a triangle"<<endl;
+            }
+
+        }
+        else
+        {
+            cout<<"invalid input"<<endl;
+        }
+        return 0;
+    }
+} // missing closing "}".
