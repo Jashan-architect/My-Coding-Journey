aff767ae9c32e297dc3082f726f9701b7375b579
Jashan <jashansindher855@gmail.com>
2026-06-12 17:13:13 +0530

    Leetcode problem number 69
    🎯Task
    find Integral part of square root of a given integer
    
    ---
    🚀Approach
    As we know that the square root of an integer is always less than it. So we can imagine it as an array of numbers less than the integer means square root <= integer so we can check it through binary search
    
    📝 Conditions
    - Integer should be positive
    
    🛠️Tech stack
    
    Language - C++
    
    Concepts used
    1. Binary Search
    2. If Else
    3. Loops
    
    ---
    
    # Coding Journey
    # C++

---
+++ square-root-using-binary-search.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int n;
+   cout<<"enter the number"<<endl;
+   cin>>n;
+
+   if(n<0){
+
+    cout<<"Invalid Input"<<endl;
+
+   }else{
+        int start = 0, end = n , ans =-1;
+        int mid = start + (end - start)/2;
+
+        
+        while(start <= end){
+
+        if(mid*mid == n){
+            ans = mid;
+            break;
+
+        }else if(mid*mid > n){
+
+            end = mid - 1;
+
+        }else{
+
+            ans = mid;
+            start = mid + 1;
+
+        }
+
+          mid = start + (end - start)/2;
+
+    }
+    cout<< ans <<endl;
+}
+}
+
+        
+   
