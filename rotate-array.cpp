6a90542a0af0b7faa9644977d7d5e77c1d5b2604
Jashan <jashansindher855@gmail.com>
2026-07-06 10:09:27 +0530

    Rotate Array
    🎯Task
    Rotate the Array K times in O(N) time complexity
    
    ---
    
    🚀Approach
    
    • Reduce Redundant Rotations: First, calculate the effective rotations using k%n, because rotating an Array of size n by n times results in the same exact same original Array.
    
    • Three - Step in - Place Reversal: Reverse the entire Array from index 0 to n - 1, then reverse the first k elements (from 0 to k -1), and finally reverse the remaining elements (from k to n -1) using a two-pointer technique to achieve O(N) time complexity.
    
    🛠️Tech Stack
    
    Language - C++
    
    Concepts used
    
    1. Vector
    2. Standard Template Library
    3. Loops
    
    ---
    
    # Coding Journey
    # C++

---
+++ rotate-array.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {1 , 2 , 3 , 4 , 5 , 6 , 7};
+
+    int n = nums.size();
+    int s = 0 , e = nums.size() - 1 ,  k = 5;
+    
+    while(s < e){
+
+        swap(nums[s] , nums[e]);
+        s++;
+        e--;
+    }
+
+    s = 0;
+    e = k- 1;
+
+    while(s < e){
+
+        swap(nums[s] , nums[e]);
+        s++;
+        e--;
+
+    }
+
+    s = k;
+    e = n - 1;
+
+    while(s < e){
+
+        swap(nums[s] , nums[e]);
+        s++;
+        e--;
+
+    }
+
+    for(int x : nums){
+
+        cout<< x <<" ";
+    }
+
+}
