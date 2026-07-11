451b422e5e5682a6d1a8eb77948f916687eda054
Jashan-architect <jashansindher855@gmail.com>
2026-07-11 17:36:45 +0530

    Fix binary search end index calculation

--- Bineary-search-II.cpp
+++ Bineary-search-II.cpp
@@ -7,7 +7,7 @@ bool searchMatrix(vector<vector<int>>& matrix, int target){
     int row = matrix.size();
     int col = matrix[0].size();
 
-    int start = 0 , end = row*col;
+    int start = 0 , end = row*col - 1;
 
     while(start <= end){
 
