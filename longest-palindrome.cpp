980cc761bfffee6791674f7ecce877078c4aa3e0
Jashan <jashansindher855@gmail.com>
2026-07-14 16:32:05 +0530

    Search Longest palindrome in the string.
    🎯Task
    Find the longest palindrome in the string.
    
    ---
    
    🚀Approach
    
    • Identify Centers for Both Odd and Even Length Palindromes: A palindrome is symmetrical around its center. Since a palindrome can be of odd length or even length, we iterate through each character of the string and treat it as a potential center for both cases.
    
    • Expand Outward Using Two Pointers: For every Chosen center index, we initialize two pointers (left and right) and expand outward in both directions simultaneously. As long as the characters at both pointers match and we stay  within the string boundaries, we continue to expand and update the maximum length of the valid palindrome substring found so far.
    
    🛠️Tech Stack
    
    Language - C++
    
    Concepts used
    
    1. String
    2. Function
    3. Loops
    4. If-Else
    
    ---
    
    # Coding Journey
    # C++

---
+++ longest-palindrome.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string expandAroundCenter(string s , int left , int right){
+
+    while(left >= 0 && right < s.size() && s[left] == s[right]){
+
+        left--;
+        right++;
+    }
+    return s.substr(left + 1 , right - left - 1);
+}
+
+string longestPalindrome(string s){
+
+    if(s.empty()){
+        return "";
+    }
+
+    string longest = "";
+
+    for(int i = 0; i < s.size(); i++){
+
+        string odd = expandAroundCenter(s , i , i);
+        if(odd.size() > longest.size()){
+
+            longest = odd;
+        }
+
+        string even = expandAroundCenter(s , i , i + 1);
+        if(even.size() > longest.size()){
+
+            longest = even;
+        }
+    }
+    return longest;
+}
+
+int main(){
+
+    string s = "dabac";
+
+    for(auto x : longestPalindrome(s)){
+        cout<<x;
+    }
+    cout<<endl;
+}
